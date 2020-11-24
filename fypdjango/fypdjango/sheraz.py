61dbbc1bfdd6803854ef123eb8d8952e9bb32923
sheri6303 <ahmedsheraz124@gmail.com>
2020-11-24 19:59:17 +0500

    last change

--- fypdjango/fypdjango/sheraz.py
+++ fypdjango/fypdjango/sheraz.py
@@ -10,4 +10,13 @@ class usersAuthenticationSerializer(serializers.ModelSerializer):
                 model = usersAuthentication
                 fields = ('name', 'username', 'department', 'password')
 
-                
+                class usersAuthenticationSerializer(serializers.ModelSerializer):
+                    class Meta:
+                        model = usersAuthentication
+                        fields = ('name', 'username', 'department', 'password')
+
+                        class usersAuthenticationSerializer(serializers.ModelSerializer):
+                            class Meta:
+                                model = usersAuthentication
+                                fields = ('name', 'username', 'department', 'password')
+
